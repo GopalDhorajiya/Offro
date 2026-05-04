788dbd19101ccba6be9a7df9442e12ac5ccb0ba0
GopalDhorajiya <gopaldhorajiya205@gmail.com>
2026-05-04 17:29:36 +0530

    fix env variable

--- offro-react/src/services/apiClient.js
+++ offro-react/src/services/apiClient.js
@@ -1,4 +1,4 @@
-const API_URL = `${process.env.VITE_BACKEND_URL}/api`;
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 let serverTimeOffset = 0;
 
--- offro-react/src/services/socketService.js
+++ offro-react/src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`${process.env.VITE_BACKEND_URL}`, {
+const socket = io(`${import.meta.env.VITE_BACKEND_URL}`, {
   reconnection: true,
   reconnectionAttempts: Infinity,
   reconnectionDelay: 1000,
