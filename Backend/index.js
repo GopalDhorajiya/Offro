de95c4efd0e14b7c4564998d889648d2ac3b061e
GopalDhorajiya <gopaldhorajiya205@gmail.com>
2026-05-04 14:00:38 +0530

    backend ready for deploy

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,9 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: "*", // we will tighten later
+}));
 app.use(express.json());
 app.use('/api/claims', claimRoutes);
 app.use('/api/shops', shopRoutes);
